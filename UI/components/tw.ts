05270b9bc71e96a886ac3126db94994700090ae9
Foodstr <136302410+foodstr@users.noreply.github.com>
2023-07-03 21:42:11 +0800

    editor ui redesign (#12)

--- UI/components/tw.ts
+++ UI/components/tw.ts
@@ -4,8 +4,7 @@ export const CenterClass = "flex items-center justify-center";
 export const NoOutlineClass = "focus:outline-none focus-visible:outline-none";
 export const inputBorderClass = `border-[2px] border-[${DividerBackgroundColor}]`;
 export const DividerClass = `h-[0.0625rem] bg-[${DividerBackgroundColor}] mt-[1.5rem] mb-[1.5rem]`;
-export const LinearGradientsClass =
-    "bg-gradient-to-r from-[#FF762C] via-[#FF3A5E] to-[#FF01A9] hover:bg-gradient-to-l";
+export const LinearGradientsClass = "bg-gradient-to-r from-[#FF762C] via-[#FF3A5E] to-[#FF01A9]";
 export const InputClass =
     `w-full px-[1rem] py-[0.75rem] rounded-lg resize-y bg-transparent ${NoOutlineClass} ${inputBorderClass} text-[${PrimaryTextColor}] placeholder-[${PlaceholderColor}]`;
 
---
+++ UI/icons2/remove-icon.tsx
@@ -0,0 +1,29 @@
+/** @jsx h */
+import { h } from "https://esm.sh/preact@10.11.3";
+
+export function RemoveIcon(props: {
+    class?: string | h.JSX.SignalLike<string | undefined> | undefined;
+    style?:
+        | string
+        | h.JSX.CSSProperties
+        | h.JSX.SignalLike<string | h.JSX.CSSProperties>
+        | undefined;
+}) {
+    return (
+        <svg
+            class={props.class}
+            style={props.style}
+            viewBox="0 0 22 22"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g>
+                <path
+                    d="M13.6669 8.33334L8.33355 13.6667M8.33355 8.33334L13.6669 13.6667M19.8891 11C19.8891 15.9092 15.9094 19.8889 11.0002 19.8889C6.09102 19.8889 2.11133 15.9092 2.11133 11C2.11133 6.09081 6.09102 2.11111 11.0002 2.11111C15.9094 2.11111 19.8891 6.09081 19.8891 11Z"
+                    stroke-width="1.77778"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+            </g>
+        </svg>
+    );
+}
