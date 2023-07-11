2d46d3f927c3545bacd427ef4821d791eb46a949
BlowaterNostr <blowater.nostr@proton.me>
2023-07-11 09:17:34 +0000

    change default relays

--- features/dm.test.ts
+++ features/dm.test.ts
@@ -1,10 +1,6 @@
 import { fail } from "https://deno.land/std@0.176.0/testing/asserts.ts";
 import { AsyncWebSocket } from "https://raw.githubusercontent.com/BlowaterNostr/nostr.ts/main/websocket.ts";
-import {
-    PrivateKey,
-    PublicKey,
-    publicKeyHexFromNpub,
-} from "https://raw.githubusercontent.com/BlowaterNostr/nostr.ts/main/key.ts";
+import { PrivateKey, PublicKey } from "https://raw.githubusercontent.com/BlowaterNostr/nostr.ts/main/key.ts";
 import {
     InMemoryAccountContext,
     NostrKind,
@@ -15,7 +11,6 @@ import {
     SingleRelayConnection,
 } from "https://raw.githubusercontent.com/BlowaterNostr/nostr.ts/main/relay.ts";
 import { getAllDecryptedMessagesOf, sendDMandImages } from "./dm.ts";
-import { relays } from "https://raw.githubusercontent.com/BlowaterNostr/nostr.ts/main/relay-list.test.ts";
 import { defaultRelays } from "../UI/setting.ts";
 
 const testPrivateKey = PrivateKey.Generate();
@@ -46,94 +41,68 @@ Deno.test("sendDMandImages", async (t) => {
     console.log(path);
     const data = await Deno.readFile(path);
 
-    // await t.step("able to send images only", async () => {
-    const errs = await sendDMandImages({
-        sender: InMemoryAccountContext.New(testPrivateKey),
-        receiverPublicKey: myPublicKey,
-        message: "   ", // will be ignored
-        files: [new Blob([data])],
-        kind: NostrKind.DIRECT_MESSAGE,
-        lamport_timestamp: 0,
-        pool,
-        waitAll: true,
-        tags: [],
-    });
-    if (errs instanceof Error) {
-        fail(errs.message);
-    }
-    if (errs) {
-        if (errs instanceof NoRelayRegistered) {
-            fail(JSON.stringify(errs));
-        } else {
-            for (const err of errs) {
-                if (err && err instanceof NoRelayRegistered) {
-                    fail(JSON.stringify(errs));
+    await t.step("able to send images only", async () => {
+        const errs = await sendDMandImages({
+            sender: InMemoryAccountContext.New(testPrivateKey),
+            receiverPublicKey: myPublicKey,
+            message: "   ", // will be ignored
+            files: [new Blob([data])],
+            kind: NostrKind.DIRECT_MESSAGE,
+            lamport_timestamp: 0,
+            pool,
+            waitAll: true,
+            tags: [],
+        });
+        if (errs instanceof Error) {
+            fail(errs.message);
+        }
+        if (errs) {
+            if (errs instanceof NoRelayRegistered) {
+                fail(JSON.stringify(errs));
+            } else {
+                for (const err of errs) {
+                    if (err && err instanceof NoRelayRegistered) {
+                        fail(JSON.stringify(errs));
+                    }
                 }
             }
         }
-    }
-    // });
+    });
 
-    // await t.step("send image and text", async () => {
-    const errs2 = await sendDMandImages({
-        sender: InMemoryAccountContext.New(testPrivateKey),
-        receiverPublicKey: myPublicKey,
-        message: "send image and text",
-        files: [new Blob([data])],
-        kind: NostrKind.DIRECT_MESSAGE,
-        lamport_timestamp: 0,
-        pool,
-        waitAll: true,
-        tags: [],
-    });
-    if (errs2 instanceof Error) {
-        fail(errs2.message);
-    }
-    if (errs2) {
-        if (errs2 instanceof NoRelayRegistered) {
-            fail(JSON.stringify(errs2));
-        } else {
-            for (const err of errs2) {
-                if (err && err instanceof NoRelayRegistered) {
-                    fail(JSON.stringify(errs2));
+    await t.step("send image and text", async () => {
+        const errs2 = await sendDMandImages({
+            sender: InMemoryAccountContext.New(testPrivateKey),
+            receiverPublicKey: myPublicKey,
+            message: "send image and text",
+            files: [new Blob([data])],
+            kind: NostrKind.DIRECT_MESSAGE,
+            lamport_timestamp: 0,
+            pool,
+            waitAll: true,
+            tags: [],
+        });
+        if (errs2 instanceof Error) {
+            fail(errs2.message);
+        }
+        if (errs2) {
+            if (errs2 instanceof NoRelayRegistered) {
+                fail(JSON.stringify(errs2));
+            } else {
+                for (const err of errs2) {
+                    if (err && err instanceof NoRelayRegistered) {
+                        fail(JSON.stringify(errs2));
+                    }
                 }
             }
         }
-    }
-    // });
+    });
 
     await pool.close();
 });
 
-// Deno.test("sendDirectMessage", async () => {
-//     const pool = new ConnectionPool();
-//     const ps = [];
-//     for (let url of relays) {
-//         const relay = SingleRelayConnection.New(url, AsyncWebSocket.New);
-//         if (relay instanceof Error) {
-//             fail(relay.message);
-//         }
-//         const p = pool.addRelay(relay);
-//         ps.push(p);
-//     }
-//     await Promise.all(ps);
-//     const eventID = await sendDirectMessage(
-//         InMemoryAccountContext.New(testPrivateKey),
-//         publicKeyFromNpub(myPublicKey),
-//         "sendDirectMessage",
-//         pool,
-//     );
-//     if (eventID instanceof Error) {
-//         fail(eventID.message);
-//     }
-//     const oks = await pool.waitEventOK(eventID, false);
-//     console.log(oks);
-//     await pool.close();
-// });
-
 Deno.test("getAllDecryptedMessagesOf", async () => {
     const pool = new ConnectionPool();
-    const relay = SingleRelayConnection.New(relays[0], AsyncWebSocket.New);
+    const relay = SingleRelayConnection.New(defaultRelays[0], AsyncWebSocket.New);
     if (relay instanceof Error) {
         fail(relay.message);
     }
