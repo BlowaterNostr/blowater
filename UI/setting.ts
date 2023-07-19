7a7d8e53a0e9d35455afc32464f38e65c94382be
BlowaterNostr <127284497+BlowaterNostr@users.noreply.github.com>
2023-07-19 16:25:36 +0800

    Sync Relay Config (#85)
    update nostr.ts

--- UI/setting.ts
+++ UI/setting.ts
@@ -1,4 +1,9 @@
-import { Database_Contextual_View } from "../database.ts";
+import {
+    NostrAccountContext,
+    prepareCustomAppDataEvent,
+} from "https://raw.githubusercontent.com/BlowaterNostr/nostr.ts/main/nostr.ts";
+import { ConnectionPool } from "https://raw.githubusercontent.com/BlowaterNostr/nostr.ts/main/relay.ts";
+import { CustomAppData, Decrypted_Nostr_Event } from "../nostr.ts";
 
 const damus = "wss://relay.damus.io";
 const nos = "wss://nos.lol";
@@ -9,6 +14,74 @@ export const defaultRelays = [
     "wss://relay.nostr.wirednet.jp",
 ];
 
-export function getRelayURLs(db: Database_Contextual_View): string[] {
-    return defaultRelays;
+export class RelayConfig {
+    private readonly relaySet = new Set<string>();
+
+    constructor(
+        public readonly pool: ConnectionPool,
+        public readonly ctx: NostrAccountContext,
+    ) {}
+
+    getRelayURLs(): string[] {
+        const urls = this.pool.getRelays().map((r) => r.url);
+        if (urls.length == 0) {
+            return defaultRelays;
+        }
+        return urls;
+    }
+
+    async addEvents(events: Decrypted_Nostr_Event[]) {
+        for (const event of events) {
+            const obj: CustomAppData = JSON.parse(event.decryptedContent);
+            if (obj.type == "AddRelay") {
+                this.relaySet.add(obj.url);
+            } else if (obj.type == "RemoveRelay") {
+                this.relaySet.delete(obj.url);
+            }
+        }
+        const s = new Set(this.pool.getRelays().map((r) => r.url));
+        // add
+        for (const url of this.relaySet) {
+            if (!s.has(url)) {
+                const err = await this.pool.addRelayURL(url);
+                if (err instanceof Error) {
+                    console.error(err);
+                    continue;
+                }
+            }
+        }
+        // remove
+        for (const url of s) {
+            if (!this.relaySet.has(url)) {
+                this.pool.removeRelay(url);
+            }
+        }
+    }
+
+    async addRelayURL(url: string) {
+        const err = await this.pool.addRelayURL(url);
+        if (err instanceof Error) {
+            return err;
+        }
+        const event = await prepareCustomAppDataEvent<CustomAppData>(this.ctx, {
+            type: "AddRelay",
+            url: url,
+        });
+        if (event instanceof Error) {
+            return event;
+        }
+        return this.pool.sendEvent(event);
+    }
+
+    async removeRelay(url: string) {
+        await this.pool.removeRelay(url);
+        const event = await prepareCustomAppDataEvent<CustomAppData>(this.ctx, {
+            type: "RemoveRelay",
+            url: url,
+        });
+        if (event instanceof Error) {
+            return event;
+        }
+        return this.pool.sendEvent(event);
+    }
 }
