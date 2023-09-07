9fe50aad964845139007a8c1730b2abe55d40411
foodstr <foodstr@proton.me>
2023-09-07 12:57:05 +0800

    popover

---
+++ UI/components/popover.test.tsx
@@ -0,0 +1,25 @@
+/** @jsx h */
+import { Fragment, h, render } from "https://esm.sh/preact@10.17.1";
+import { Popover, showPopover,  } from "./popover.tsx";
+
+function PopoverTest() {
+    return (
+        <Fragment>
+            <button onClick={() => {
+                showPopover(() => {
+                    console.log("show");
+                });
+            }}>show</button>
+            {
+                Popover({
+                    children: <h1>popover simple</h1>,
+                    onHide: () => {
+                        console.log("hide");
+                    }
+                })
+            }
+        </Fragment>
+    )
+}
+
+render(PopoverTest(), document.body);
--- UI/components/popover.tsx
+++ UI/components/popover.tsx
@@ -1,40 +1,66 @@
 /** @jsx h */
-import { ComponentChildren, h } from "https://esm.sh/preact@10.17.1";
+import { ComponentChildren, Fragment, h } from "https://esm.sh/preact@10.17.1";
 import { tw } from "https://esm.sh/twind@0.16.16";
 import { SecondaryBackgroundColor } from "../style/colors.ts";
+import { computed, signal } from "https://esm.sh/@preact/signals@1.2.1";
+
+// Model
+const popoverStatus = signal<"show" | "hide">("hide");
+
+export function showPopover(onShow?: () => void) {
+    popoverStatus.value = "show";
+    if (onShow) {
+        onShow();
+    }
+}
+
+export function hidePopover(onHide?: () => void) {
+    popoverStatus.value = "hide";
+    if (onHide) {
+        onHide();
+    }
+}
+
+function onEscKeyDown(e: h.JSX.TargetedKeyboardEvent<HTMLDivElement>, onHide?: () => void) {
+    if ( e.code != "Escape") {
+        return;
+    }
+
+    hidePopover(onHide);
+}
+
+// view
+const popoverStyle = {
+    container: tw`fixed inset-0 z-20`,
+    backdrop: tw`fixed inset-0 z-[-1] backdrop-filter backdrop-blur`,
+    childrenContainer: tw`h-[80%] absolute top-[20%] overflow-auto bg-[${SecondaryBackgroundColor}] w-full shadow-lg`,
+}
 
 export function Popover(props: {
-    escapeClose?: boolean;
-    blankClickClose?: boolean;
-    close: () => void;
-    children: ComponentChildren;
+    children: ComponentChildren,
+    onHide?: () => void,
 }) {
-    return (
-        <div
-            class={tw`fixed inset-0 z-20`}
-            onKeyDown={(e) => {
-                if (props.escapeClose && e.code === "Escape") {
-                    props.close();
-                }
-            }}
-        >
-            <div
-                class={tw`fixed inset-0 z-[-1] backdrop-filter backdrop-blur`}
-                onClick={props.blankClickClose
-                    ? () => {
-                        props.close();
-                    }
-                    : undefined}
-            >
-            </div>
-            <div
-                class={tw`h-[80%] absolute top-[20%] overflow-auto bg-[${SecondaryBackgroundColor}] w-full`}
-                style={{
-                    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-                }}
-            >
-                {props.children}
-            </div>
-        </div>
-    );
+    const { children, onHide } = props;
+
+    const popoverView = computed(() => (
+        popoverStatus.value == "show"
+            ?   <div
+                    class={popoverStyle.container}
+                    onKeyDown={(e) => onEscKeyDown(e, onHide)}
+                >
+                    <div
+                        class={popoverStyle.backdrop}
+                        onClick={() => hidePopover(onHide)}
+                    >
+                    </div>
+                    <div
+                        class={popoverStyle.childrenContainer}
+                    >
+                        {children}
+                    </div>
+                </div>
+            : <Fragment></Fragment>
+    ));
+
+    return popoverView;
 }
