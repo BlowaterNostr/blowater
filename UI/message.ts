01da7afea51a035783f6aaebb2c33b19a8f10b07
pqingshuang <26760878+shaibearary@users.noreply.github.com>
2023-09-11 10:27:27 +0800

    forma

--- UI/message.ts
+++ UI/message.ts
@@ -105,7 +105,7 @@ export type ContentItem = {
     pubkey: string;
     start: number;
     end: number;
-    relays?: string[] ;
+    relays?: string[];
 } | {
     type: "note";
     noteID: NoteID;
