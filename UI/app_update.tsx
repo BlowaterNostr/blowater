06802fe8ea21161e273f42ae25b00bcb39b4dc60
BlowaterNostr <127284497+BlowaterNostr@users.noreply.github.com>
2023-09-13 18:24:42 +0000

    Update app_update.tsx

--- UI/app_update.tsx
+++ UI/app_update.tsx
@@ -521,15 +521,12 @@ export async function* Database_Update(
         }
 
         let hasKind_1 = false;
+        profileSyncer.add(...changes_events.map(e=>e.pubkey));
         for (let e of changes_events) {
             allUserInfo.addEvents([e]);
             const t = getTags(e).lamport_timestamp;
             if (t) {
                 lamport.set(t);
-            }
-            const key = PublicKey.FromHex(e.pubkey);
-            if (key instanceof PublicKey) {
-                profileSyncer.add(key.hex);
             }
             if (e.kind == NostrKind.META_DATA || e.kind == NostrKind.DIRECT_MESSAGE) {
                 for (const contact of allUserInfo.userInfos.values()) {
