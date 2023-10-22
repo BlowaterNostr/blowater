824d5f435af14929abe18429a3e8cd345d173452
BlowaterNostr <127284497+BlowaterNostr@users.noreply.github.com>
2023-10-22 17:02:31 +0800

    Update profile-card.test.tsx

--- UI/profile-card.test.tsx
+++ UI/profile-card.test.tsx
@@ -1,29 +1,18 @@
 /** @jsx h */
-import { Fragment, h, render } from "https://esm.sh/preact@10.17.1";
+import { h, render } from "https://esm.sh/preact@10.17.1";
 import { PrivateKey } from "../lib/nostr-ts/key.ts";
 import { InMemoryAccountContext, NostrKind } from "../lib/nostr-ts/nostr.ts";
-import { parseJSON } from "../features/profile.ts";
-import { fail } from "https://deno.land/std@0.176.0/testing/asserts.ts";
 import { ProfileCard } from "./profile-card.tsx";
-import { prepareNormalNostrEvent } from "../lib/nostr-ts/event.ts";
 import { testEventBus } from "./_setup.test.ts";
 
 const ctx = InMemoryAccountContext.New(PrivateKey.Generate());
-const profileEvent = await prepareNormalNostrEvent(
-    ctx,
-    NostrKind.META_DATA,
-    [],
-    `{"name":"mike", "about": "a test man"}`,
-);
-
-const profileData = parseJSON(profileEvent.content);
-if (profileData instanceof Error) {
-    fail(profileData.message);
-}
 
 render(
     <div>
-        <ProfileCard publicKey={ctx.publicKey} profileData={profileData} emit={testEventBus.emit} />
+        <ProfileCard publicKey={ctx.publicKey} profileData={{
+            about: "a nostr user",
+            name: "Mike",
+        }} emit={testEventBus.emit} />
         <ProfileCard publicKey={ctx.publicKey} profileData={undefined} emit={testEventBus.emit} />
         <ProfileCard
             publicKey={ctx.publicKey}
