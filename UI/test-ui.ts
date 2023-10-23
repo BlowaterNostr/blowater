c1137aee549ba65c376c2233254fcd19abc6d6f7
qspang <26760878+shaibearary@users.noreply.github.com>
2023-10-23 18:58:54 +0800

    fmt

--- UI/test-ui.ts
+++ UI/test-ui.ts
@@ -1,35 +1,31 @@
-
-
 import { walk } from "https://deno.land/std@0.202.0/fs/walk.ts";
 
 import { bundle } from "https://deno.land/x/emit/mod.ts";
 async function findTestFiles(directory: string): string[] {
     const testFiles: string[] = [];
-  
+
     for await (const entry of walk(directory, { exts: [".test.tsx"] })) {
-      testFiles.push(entry.path);
+        testFiles.push(entry.path);
     }
-  
+
     return testFiles;
-  }
+}
 
 const testFiles = await findTestFiles("./");
 const promiseArray = testFiles.map(async (param) => {
-
-
     // const command = `deno bundle --config=./deno.json ${param} deploy/main.mjs`;
-  
+
     // // Execute the command asynchronously
     // const process = Deno.run({
     //   cmd: command.split(" "),
     //   stdout: "piped",
     //   stderr: "piped",
     // });
-  
+
     // const { code } = await process.status();
     // const output = new TextDecoder().decode(await process.output());
     // const error = new TextDecoder().decode(await process.stderrOutput());
-  
+
     // console.log(`Command: ${command}`);
     // console.log(`Exit Code: ${code}`);
     // console.log(`Output: ${output}`);
@@ -38,13 +34,11 @@ const promiseArray = testFiles.map(async (param) => {
 
     // import { bundle } from "https://deno.land/x/emit/mod.ts";
     const result = await bundle(
-    new URL(param,import.meta.url),
+        new URL(param, import.meta.url),
     );
 
     const { code } = result;
     console.log(code);
-  });
+});
 
-
-  
 await Promise.all(promiseArray);
