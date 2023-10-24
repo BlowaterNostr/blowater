ef7735718a27ef5d08a1f31961434821286c1ac0
BlowaterNostr <127284497+BlowaterNostr@users.noreply.github.com>
2023-10-24 11:04:31 +0800

    fix

--- UI/test-ui.ts
+++ UI/test-ui.ts
@@ -1,7 +1,7 @@
 import { walk } from "https://deno.land/std@0.202.0/fs/walk.ts";
+import { bundle } from "https://deno.land/x/emit@0.31.0/mod.ts";
 
-import { bundle } from "https://deno.land/x/emit/mod.ts";
-async function findTestFiles(directory: string): string[] {
+async function findTestFiles(directory: string): Promise<string[]> {
     const testFiles: string[] = [];
 
     for await (const entry of walk(directory, { exts: [".test.tsx"] })) {
@@ -13,26 +13,6 @@ async function findTestFiles(directory: string): string[] {
 
 const testFiles = await findTestFiles("./");
 const promiseArray = testFiles.map(async (param) => {
-    // const command = `deno bundle --config=./deno.json ${param} deploy/main.mjs`;
-
-    // // Execute the command asynchronously
-    // const process = Deno.run({
-    //   cmd: command.split(" "),
-    //   stdout: "piped",
-    //   stderr: "piped",
-    // });
-
-    // const { code } = await process.status();
-    // const output = new TextDecoder().decode(await process.output());
-    // const error = new TextDecoder().decode(await process.stderrOutput());
-
-    // console.log(`Command: ${command}`);
-    // console.log(`Exit Code: ${code}`);
-    // console.log(`Output: ${output}`);
-    // console.log(`Error: ${error}`);
-    // if (error!=null) {throw(error)}
-
-    // import { bundle } from "https://deno.land/x/emit/mod.ts";
     const result = await bundle(
         new URL(param, import.meta.url),
     );
