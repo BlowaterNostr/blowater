1f837f66fc58de97946b28e72f005b4c05232ecc
foodstr <foodstr@proton.me>
2023-10-30 10:27:22 +0800

    extension-v2

---
+++ UI/deploy.ts
@@ -0,0 +1,5 @@
+import { bundle } from "https://deno.land/x/emit@0.31.0/mod.ts";
+
+const url = new URL("./_main.tsx", import.meta.url);
+const res = await bundle(url);
+await Deno.writeTextFile("./deploy/main.mjs", res.code);
