b176b57afcebe674565c1aff8addc002babcbff3
BlowaterNostr <127284497+BlowaterNostr@users.noreply.github.com>
2023-11-20 15:28:06 +0800

    Update app.tsx

--- UI/app.tsx
+++ UI/app.tsx
@@ -78,7 +78,6 @@ export async function Start(database: DexieDatabase) {
         })
     ) {
         const t = Date.now();
-        console.log("rener");
         {
             render(
                 AppComponent({
