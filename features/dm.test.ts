a85218a84cf69317b3ed9f3e0e75f324a940a0f4
BlowaterNostr <127284497+BlowaterNostr@users.noreply.github.com>
2023-11-22 14:33:18 +0800

    Update dm.test.ts

--- features/dm.test.ts
+++ features/dm.test.ts
@@ -15,7 +15,7 @@ Deno.test("DirectedMessageController", async () => {
         // get an observable channel before adding events
         const chan = dmController.getDirectMessageStream(ctx.publicKey.hex);
         {
-            // add events
+            // prepare events
             const events = [];
             for (let i = 1; i <= 3; i++) {
                 const event = await prepareEncryptedNostrEvent(ctx, {
@@ -30,29 +30,32 @@ Deno.test("DirectedMessageController", async () => {
 
                 events.push(event);
             }
+
+            // add 2 events
             await dmController.addEvent(events[0]);
             await dmController.addEvent(events[1]);
 
-            // get 2 events
-            const new_messages = dmController.getChatMessages(ctx.publicKey.hex);
-            assertEquals(new_messages.map((m) => m.content), ["test:1", "test:2"]);
+            // get 0 events because of lazy loading
+            let messages = dmController.getChatMessages(ctx.publicKey.hex);
+            assertEquals(messages.map((m) => m.content), []);
 
-            // the channel is able to retrieve all events
+            // the channel is able to retrieve 2 events
             const message = await chan.pop() as ChatMessage;
             assertEquals(message.content, "test:1");
 
             const message2 = await chan.pop() as ChatMessage;
             assertEquals(message2.content, "test:2");
 
-            // get 3 events
-            {
-                await dmController.addEvent(events[2]);
+            // get 1 more event
+            await dmController.addEvent(events[2]);
 
-                const new_messages = dmController.getChatMessages(ctx.publicKey.hex);
-                assertEquals(new_messages.map((m) => m.content), ["test:1", "test:2", "test:3"]);
+            // get 2 events of because of lazy loading
+            const new_messages = dmController.getChatMessages(ctx.publicKey.hex);
+            assertEquals(new_messages.map((m) => m.content), ["test:1", "test:2"]);
 
-                assertEquals((await chan.pop() as ChatMessage).content, "test:3");
-            }
+            // retrieve the 3rd event
+            assertEquals((await chan.pop() as ChatMessage).content, "test:3");
+
         }
         await chan.close(); // not necessary
     }
