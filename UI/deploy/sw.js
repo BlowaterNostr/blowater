3f08f392f1027ae3870e71806e202c8fad9d532f
foodstr <foodstr@proton.me>
2023-11-24 14:49:45 +0800

    +

--- UI/deploy/sw.js
+++ UI/deploy/sw.js
@@ -8,50 +8,33 @@ const contentToCache = [
     "./main.mjs",
 ];
 
-const putInCache = async (request, response) => {
-    const cache = await caches.open(cacheName);
-    await cache.put(request, response);
-};
-
 self.addEventListener("install", (e) => {
     e.waitUntil(
         (async () => {
             const cache = await caches.open(cacheName);
-            console.log("[Service Worker] Caching all content");
             await cache.addAll(contentToCache);
         })(),
     );
 });
 
-const cacheFirst = async ({ request, fallbackUrl }) => {
-    // First try to get the resource from the cache.
+const putInCache = async (request, response) => {
+    const cache = await caches.open(cacheName);
+    await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request }) => {
     const responseFromCache = await caches.match(request);
     if (responseFromCache) {
         return responseFromCache;
     }
 
-    // If the response was not found in the cache,
-    // try to get the resource from the network.
     try {
         const responseFromNetwork = await fetch(request);
-        // If the network request succeeded, clone the response:
-        // - put one copy in the cache, for the next time
-        // - return the original to the app
-        // Cloning is needed because a response can only be consumed once.
         putInCache(request, responseFromNetwork.clone());
         return responseFromNetwork;
     } catch (error) {
-        // If the network request failed,
-        // get the fallback response from the cache.
-        const fallbackResponse = await caches.match(fallbackUrl);
-        if (fallbackResponse) {
-            return fallbackResponse;
-        }
-        // When even the fallback response is not available,
-        // there is nothing we can do, but we must always
-        // return a Response object.
-        return new Response("Network error happened", {
-            status: 408,
+        return new Response(error.message ? error.message: error, {
+            status: 500,
             headers: { "Content-Type": "text/plain" },
         });
     }
@@ -61,7 +44,6 @@ self.addEventListener("fetch", (event) => {
     event.respondWith(
         cacheFirst({
             request: event.request,
-            fallbackUrl: "./index.html",
         }),
     );
 });
