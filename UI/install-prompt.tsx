00022f846d9930a9341d3a7ac90a2247da39e020
foodstr <foodstr@proton.me>
2023-11-27 10:21:12 +0800

    +

--- UI/install-prompt.tsx
+++ UI/install-prompt.tsx
@@ -2,9 +2,9 @@
 import { h } from "https://esm.sh/preact@10.11.3";
 import { Component } from "https://esm.sh/preact@10.17.1";
 import { tw } from "https://esm.sh/twind@0.16.16";
-import { PrimaryBackgroundColor, PrimaryTextColor } from "./style/colors.ts";
+import { PrimaryTextColor } from "./style/colors.ts";
 import { DownloadIcon } from "./icons/download-icon.tsx";
-import { IconButtonClass } from "./components/tw.ts";
+import { IconButtonClass, LinearGradientsClass } from "./components/tw.ts";
 import { CloseIcon } from "./icons/close-icon.tsx";
 
 type State = {
@@ -14,9 +14,9 @@ type State = {
 export class InstallPrompt extends Component<{}, State> {
     styles = {
         container:
-            tw`absolute bottom-4 mobile:bottom-20 bg-[${PrimaryBackgroundColor}] max-w-sm z-30 right-4 px-4 py-2 rounded text-[${PrimaryTextColor}] flex cursor-pointer`,
-        icon: tw`text-[${PrimaryTextColor}] stroke-current w-6 h-6 mr-2 mobile:mr-0`,
-        text: tw`mobile:hidden  hover:underline`,
+            tw`absolute bottom-4 mobile:bottom-20 ${LinearGradientsClass} max-w-sm z-30 right-4 px-4 py-2 rounded text-[${PrimaryTextColor}] flex cursor-pointer`,
+        icon: tw`text-[${PrimaryTextColor}] stroke-current w-6 h-6 mr-2`,
+        text: tw`hover:underline`,
         close: tw`${IconButtonClass} w-6 h-6 ml-2 -top-1.5 -right-1.5 bg-[#42464D] absolute`,
         closeIcon: tw`text-[${PrimaryTextColor}] stroke-current w-4 h-4`,
     };
