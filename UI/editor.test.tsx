dc31f497ce3b69479e422476518d13664b2913f8
BlowaterNostr <127284497+BlowaterNostr@users.noreply.github.com>
2023-12-22 14:46:02 +0800

    +

--- UI/editor.test.tsx
+++ UI/editor.test.tsx
@@ -8,22 +8,16 @@ import { testEventBus } from "./_setup.test.ts";
 const ctx = InMemoryAccountContext.New(PrivateKey.Generate());
 
 let vdom = (
-    <Editor
-        placeholder="Message @xxx"
-        maxHeight="50vh"
-        eventEmitter={testEventBus}
-        model={{
-            files: [],
-            id: "",
-            tags: [],
-            target: {
-                kind: NostrKind.DIRECT_MESSAGE,
-                receiver: {
-                    pubkey: ctx.publicKey,
-                },
-            },
-            text: "",
-        }}
-    />
+    <div class="border">
+        <Editor
+            placeholder="Message @xxx"
+            maxHeight="50vh"
+            emit={testEventBus.emit}
+            files={[]}
+            isGroupChat={false}
+            targetNpub={ctx.publicKey}
+            text="This is a test"
+        />
+    </div>
 );
 render(vdom, document.body);
