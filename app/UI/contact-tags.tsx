88d587e3659823267aa9aaf8dea0fcc372a0941b
BlowaterNostr <127284497+BlowaterNostr@users.noreply.github.com>
2024-01-02 17:11:48 +0800

    Update contact-tags.tsx

--- app/UI/contact-tags.tsx
+++ app/UI/contact-tags.tsx
@@ -14,43 +14,36 @@ type Props = {
 };
 
 type State = {
-    tags: {
-        name: string;
-        selected: boolean;
-    }[];
+    selectedTag: string;
 };
 
 export class ContactTags extends Component<Props, State> {
     state: State = {
-        tags: this.props.tags.map((tag) => ({
-            name: tag,
-            selected: false,
-        })),
+        selectedTag: "",
     };
 
     render() {
         return (
             <div>
                 {/* https://tailwindcolor.com/ */}
-                {Array.from(this.state.tags).map((tag) => (
+                {Array.from(this.props.tags).map((tag) => (
                     <div
                         class={`m-1 px-2 rounded-full inline-block
                             hover:cursor-pointer select-none
                             text-white hover:text-black
-                            ${tag.selected ? "border-2 border-cyan-300" : ""}
+                            ${tag == this.state.selectedTag ? "border-2 border-cyan-300" : ""}
                             bg-green-400 hover:bg-white`}
                         onClick={(e) => {
-                            tag.selected = !tag.selected;
                             this.setState({
-                                tags: this.state.tags,
+                                selectedTag: tag,
                             });
                             this.props.emit({
                                 type: "tagSelected",
-                                tag: tag.name,
+                                tag: tag,
                             });
                         }}
                     >
-                        {tag.name}
+                        {tag}
                     </div>
                 ))}
             </div>
