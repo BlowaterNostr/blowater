2b0f025256269adaa729ef2903f36c5bab636b8b
bob2402 <dv4xd@proton.me>
2024-02-27 17:21:03 +0800

    test: update channel-container

--- app/UI/channel-container.test.tsx
+++ app/UI/channel-container.test.tsx
@@ -1,4 +1,21 @@
 import { h, render } from "https://esm.sh/preact@10.17.1";
 import { ChannelContainer } from "./channel-container.tsx";
+import { relays } from "../../libs/nostr.ts/relay-list.test.ts";
+import { ConnectionPool } from "../../libs/nostr.ts/relay-pool.ts";
+import { testEventBus } from "./_setup.test.ts";
+import { SingleRelayConnection } from "../../libs/nostr.ts/relay-single.ts";
 
-render(<ChannelContainer />, document.body);
+const pool = new ConnectionPool();
+pool.addRelayURL(relays[0]);
+
+const relay = pool.getRelay(relays[0]) as SingleRelayConnection;
+
+render(
+    <ChannelContainer
+        relay={relay}
+        bus={testEventBus}
+        currentChannel={undefined}
+        relaySelectedChannel={new Map()}
+    />,
+    document.body,
+);
