8a7a989bddc2d553d4b353bd002c37d5a14d68fd
BlowaterNostr <blowater.nostr@proton.me>
2024-03-07 18:41:17 +0800

    Update channel-list.test.tsx

--- app/UI/channel-list.test.tsx
+++ app/UI/channel-list.test.tsx
@@ -2,8 +2,13 @@ import { h, render } from "https://esm.sh/preact@10.17.1";
 import { ChannelList } from "./channel-list.tsx";
 import { testEventBus } from "./_setup.test.ts";
 
-render(<ChannelList
-    emit={testEventBus.emit}
-    relay="test"
-    currentSelected="test"
-    channels={["general", "games", "work"]}></ChannelList>, document.body);
+render(
+    <ChannelList
+        emit={testEventBus.emit}
+        relay="test"
+        currentSelected="test"
+        channels={["general", "games", "work"]}
+    >
+    </ChannelList>,
+    document.body,
+);
