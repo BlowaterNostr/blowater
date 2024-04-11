4150949e395259c20451f979894c80ea358c5316
Bob <dv4xd@proton.me>
2024-04-11 14:18:50 +0800

    test: switch between reply mode and normal mode

--- app/UI/editor.test.tsx
+++ app/UI/editor.test.tsx
@@ -1,5 +1,6 @@
 /** @jsx h */
 import { h, render } from "https://esm.sh/preact@10.17.1";
+import { useState } from "https://esm.sh/preact@10.17.1/hooks";
 import { Editor } from "./editor.tsx";
 import { testEventBus } from "./_setup.test.ts";
 import { InMemoryAccountContext, NostrKind } from "../../libs/nostr.ts/nostr.ts";
@@ -8,6 +9,7 @@ import { Database_View } from "../database.ts";
 import { prepareEncryptedNostrEvent } from "../../libs/nostr.ts/event.ts";
 import { fail } from "https://deno.land/std@0.176.0/testing/asserts.ts";
 import { NostrEvent } from "../../libs/nostr.ts/nostr.ts";
+import { NoteID } from "../../libs/nostr.ts/nip19.ts";
 
 const indexedDB = NewIndexedDB();
 if (indexedDB instanceof Error) {
@@ -16,7 +18,7 @@ if (indexedDB instanceof Error) {
 const database = await Database_View.New(indexedDB, indexedDB, indexedDB);
 
 const ctx = InMemoryAccountContext.Generate();
-const e = await database.addEvent(
+const event = await database.addEvent(
     await prepareEncryptedNostrEvent(ctx, {
         encryptKey: ctx.publicKey,
         kind: NostrKind.DIRECT_MESSAGE,
@@ -24,27 +26,41 @@ const e = await database.addEvent(
         content: "hi",
     }) as NostrEvent,
 );
-if (!e || e instanceof Error) {
-    fail();
+
+function EditorText() {
+    if (!event || event instanceof Error) {
+        fail();
+    }
+    const [eventID, setEventID] = useState<string | NoteID | undefined>(undefined);
+
+    return (
+        <div class="h-full w-full flex-col justify-center items-center gap-2 bg-black">
+            <div class="w-full text-center">
+                <button
+                    class="w-20 h-10 rounded bg-black px-4 py2 bg-white"
+                    onClick={() => setEventID(event.id)}
+                >
+                    Reply
+                </button>
+            </div>
+
+            <Editor
+                replyTo={{
+                    eventID: eventID,
+                    onEventIDChange: (id) => {
+                        setEventID(id);
+                    },
+                }}
+                placeholder="Message @xxx"
+                maxHeight="50vh"
+                emit={testEventBus.emit}
+                getters={{
+                    profileGetter: database,
+                    getEventByID: database.getEventByID,
+                }}
+            />
+        </div>
+    );
 }
 
-let vdom = (
-    <div class="border">
-        <Editor
-            replyTo={{
-                eventID: e.id,
-                onEventIDChange: (id) => {
-                    console.log(`onReplyToEventIDChange: ${id}`);
-                },
-            }}
-            placeholder="Message @xxx"
-            maxHeight="50vh"
-            emit={testEventBus.emit}
-            getters={{
-                profileGetter: database,
-                getEventByID: database.getEventByID,
-            }}
-        />
-    </div>
-);
-render(vdom, document.body);
+render(<EditorText />, document.body);
