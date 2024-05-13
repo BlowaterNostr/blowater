75f873e676c11e16185237301136669ba5bc9d36
Bob <dv4xd@proton.me>
2024-05-13 22:29:38 +0800

    clean import

--- app/UI/app_model.ts
+++ app/UI/app_model.ts
@@ -3,7 +3,7 @@ import { DM_Model } from "./dm.tsx";
 import { Public_Model } from "./public-message-container.tsx";
 import { App } from "./app.tsx";
 import { default_blowater_relay } from "./relay-config.ts";
-import { getRelayInformation, RelayInformation } from "./relay-detail.tsx";
+import { RelayInformation } from "./relay-detail.tsx";
 
 export type Model = {
     app?: App; // app is only available after sign-in
